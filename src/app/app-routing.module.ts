2b49e19dc29d3fb7aa04f6338971ac07134320d5
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-20 12:49:42 -0600

    Add project scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'locations',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.module').then(
+        m => m.DashboardModule,
+      ),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.module').then(m => m.AdminModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'locations',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
