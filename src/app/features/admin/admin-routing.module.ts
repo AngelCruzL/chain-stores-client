2d31a7671310802676138b73384e18a769d0187e
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-21 19:30:48 -0600

    Add admin pages routes

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+import { AdminComponent } from './admin.component';
+import { ManageStoresComponent } from './pages/manage-stores/manage-stores.component';
+import { AddNewStoreComponent } from './pages/add-new-store/add-new-store.component';
+import { EditStoreComponent } from './pages/edit-store/edit-store.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      { path: '', component: ManageStoresComponent },
+      { path: 'new-store', component: AddNewStoreComponent },
+      { path: 'edit-store/:id', component: EditStoreComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-
+import { ManageStoresComponent } from './pages/manage-stores/manage-stores.component';
+import { AddNewStoreComponent } from './pages/add-new-store/add-new-store.component';
+import { EditStoreComponent } from './pages/edit-store/edit-store.component';
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    ManageStoresComponent,
+    AddNewStoreComponent,
+    EditStoreComponent,
   ],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  imports: [CommonModule, AdminRoutingModule, ReactiveFormsModule],
 })
-export class AdminModule { }
+export class AdminModule {}
