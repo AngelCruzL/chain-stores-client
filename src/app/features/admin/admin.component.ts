9869d7d3ff9833739dd2eaf90e8d9fadf21d4f77
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-21 19:25:40 -0600

    Add logout functionality

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
+  #router = inject(Router);
 
+  logout() {
+    localStorage.removeItem('token');
+    this.#router.navigate(['/auth/login']);
+  }
 }
